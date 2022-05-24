2a4c9233b8a5039f55bbf17432a31cd076774b70
Jorge Ariztegui <jorgeariztegui8@gmail.com>
2022-05-24 13:59:42 -0300

    Se agrego styled components

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,12 +1,51 @@
+
 import React from 'react'
-import characters from '../characters.json'
+import characters from '../characters.json';
+import styled from 'styled-components';
+
+const OcultarInfo = styled.div`
+  position: absolute;
+  text-align: center;
+  display: none;
+`;
+
+const Title = styled.p`
+  font-size: 25px;
+  color: #fff;
+  font-weight: 600;
+  margin: 10px 0;
+`;
+
+const ContainerBlogImg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`;
+
+const Imagen = styled.img`
+  transition: 0.5s;
+  width: 100%;
+
+  &:hover{
+    filter: brightness(0.3);
+    cursor: pointer;
+  }
+`;
 
 const Blog = () => {
   return (
     <>
-         <div className='container-blog'>
+         <div className='p-2 container-blog'>
           {characters.map(e =>(
-              <img className='col-4' src={e.img} alt="img" />
+            <ContainerBlogImg className='col-4 p-2'>
+              <Imagen src={e.img} alt="img"/>
+                <OcultarInfo>
+                  <Title>{e.nombre}</Title>
+                  <button className='btn btn-secondary'>Ver más</button>
+                </OcultarInfo>
+            </ContainerBlogImg>
           ))}
          </div>
     </>
